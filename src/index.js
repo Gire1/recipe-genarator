ead50e75e15ed836b066386e090d414396991731
Gire1 <168452060+Gire1@users.noreply.github.com>
2025-12-22 21:41:47 +0200

    Added basic java script and type writer

--- src/index.js
+++ src/index.js
@@ -1,1 +1,14 @@
-alert("hello world");
+function generateRecipe(event){
+event.preventDefault();
+
+new Typewriter("recipe", {
+  strings: ["Explore thousands of tested recipes"],
+  autoStart: true,
+  delay:1,
+  cursor:""
+});
+
+}
+
+let recipeFormElement =document.querySelector("#recipe-generator-form")
+recipeFormElement.addEventListener("submit", generateRecipe);
