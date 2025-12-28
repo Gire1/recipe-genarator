e7fb7b1be3ae8085c405b74b0a5709285fcf6852
Gire1 <168452060+Gire1@users.noreply.github.com>
2025-12-28 14:56:55 +0200

    Improved the Use Experience

--- src/index.js
+++ src/index.js
@@ -16,7 +16,14 @@ let apiKey="3f5te44f20dc10969a23fb1a9afo8c90";
 let context=
 "You are a creative food expert and love to share recipes.Your mission is to genrate a simple recipe in basic HTML.Make sure to follow the user instructions "
 let prompt = `User Instructions:Generate a recipe about ${instructionsInput.value}`;
-let apiURL=`https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
+let apiURL =`https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
+
+let recipeElement=document.querySelector("recipe")
+recipeElement.classList.remove("hidden");
+recipeElement.innerHTML =
+  <div class="text-style blink-smooth">Smooth Blink Text
+`Generating a recipe about ${instructionsInput.Value}`</div>;
+
 
 console.log("Generating recipe");
  console.log(`Prompt:${prompt}`);
