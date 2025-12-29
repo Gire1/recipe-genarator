4c3026a3417b2da55d96590388edac8316644564
Gire1 <168452060+Gire1@users.noreply.github.com>
2025-12-29 20:39:08 +0200

    Updated index.js

--- src/index.js
+++ src/index.js
@@ -1,41 +1,27 @@
-function  displayRecipe(response){
- new Typewriter("recipe", {
+function displayRecipe(response) {
+  new Typewriter("#recipe", {
     strings: response.data.answer,
     autoStart: true,
     delay: 1,
     cursor: "",
   });
-
 }
 
-function generateRecipe(event){
-event.preventDefault();
-let instructionsInput=document.querySelector("#user-instructions")
-let apiKey="3f5te44f20dc10969a23fb1a9afo8c90";
-let context=
-"You are a creative food expert and love to share recipes.Your mission is to genrate a simple recipe in basic HTML.Make sure to follow the user instructions "
-let prompt = `User Instructions:Generate a recipe about ${instructionsInput.value}`;
-let apiURL =`https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
+function generateRecipe(event) {
+  event.preventDefault();
+  let instructionsInput = document.querySelector("#user-instructions");
+  let apiKey = "3f5te44f20dc10969a23fb1a9afo8c90";
+  let context =
+    "You are a creative food expert and love to share recipes.Your mission is to genrate a simple recipe in basic HTML.Make sure to follow the user instructions ";
+  let prompt = `User Instructions:Generate a recipe about ${instructionsInput.value}`;
+  let apiURL = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
 
-let recipeElement=document.querySelector("recipe")
-recipeElement.classList.remove("hidden");
-recipeElement.innerHTML =`
-  <div class="text-style blink-smooth">Smooth Blink Text
-Generating a recipe about ${instructionsInput.Value}</div>`;
+  let recipeElement = document.querySelector("#recipe");
+  recipeElement.classList.remove("hidden");
+  recipeElement.innerHTML = `
+  <div class="text-style blink-smooth">Smooth Blink Text Generating a recipe about ${instructionsInput.value}</div>`;
 
-
-
-axios.get(UrL).then(displayRecipe);
-  }
-
-new Typewriter("recipe", {
-  strings: "Explore thousands of tested recipes",
-  autoStart: true,
-  delay:1,
-  cursor:"",
-});
-
-
-
+  axios.get(apiURL).then(displayRecipe);
+}
 let recipeFormElement = document.querySelector("#recipe-generator-form");
 recipeFormElement.addEventListener("submit", generateRecipe);
